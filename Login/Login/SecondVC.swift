259231cb0d2eb324940202cc3d8468e941625eb2
DianaDmitriev4 <qwertyuiop25852@mail.ru>
2023-11-11 17:33:50 +0700

    repeat

---
+++ Login/Login/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  Login
+//
+//  Created by User on 10.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class SecondVC: UIViewController {
+
+    
+    
+    
+    
+    
+}
